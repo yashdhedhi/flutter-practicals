9cfac21bc386858520c1e44cf40f5e416c39ff85
yashdhedhi <yash.dhedhi120222@marwadiuniversity.ac.in>
2025-04-16 19:41:49 +0530

    Update main.dart

--- Practical_01/main.dart
+++ Practical_01/main.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('Hello World! Your setup is complete.'),
+      ),
+    );
+  }
+}
 
